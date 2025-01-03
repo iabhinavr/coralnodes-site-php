8d52daf4cebe760a2a5562f74a30778438434657
Abhinav R <abhinav7r@gmail.com>
2025-01-04 02:26:58 +0530

    homepage edited

--- app/public/views/home-page.php
+++ app/public/views/home-page.php
@@ -1,6 +1,6 @@
 <article class="single-post-article" id="article-<?= $props['id'] ?>">
-    <div class="post-content container mx-auto lg:max-w-3xl px-2 tracking-tight">
-        
+    <div class="post-content container mx-auto lg:max-w-3xl px-2 tracking-tight mt-6">
+        <h1>How to Make Content Websites in 2025? My Thoughts</h1>
         <div class="py-4">
             <?= $props['content_html'] ?>
         </div>
